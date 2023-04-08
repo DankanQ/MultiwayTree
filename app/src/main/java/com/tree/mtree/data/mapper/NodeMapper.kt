72a6149cb2f02ed0a49d3bf09174e6f590793584
Yoru <taihoudev@gmail.com>
2023-04-08 22:30:13 +0300

    #6-add-mapper

---
+++ app/src/main/java/com/tree/mtree/data/mapper/NodeMapper.kt
@@ -0,0 +1,32 @@
+package com.tree.mtree.data.mapper
+
+import com.tree.mtree.data.database.model.NodeDbModel
+import com.tree.mtree.domain.model.Node
+
+class NodeMapper {
+    fun mapModelToDbModel(node: Node) = NodeDbModel(
+        id = node.id,
+        name = node.name,
+        children = mapNodesModelToDbModel(node.children),
+        parentId = node.parentId
+    )
+
+    fun mapDbModelToModel(nodeDbModel: NodeDbModel) = Node(
+        id = nodeDbModel.id,
+        name = nodeDbModel.name,
+        children = mapNodesDbModelToModel(nodeDbModel.children),
+        parentId = nodeDbModel.parentId
+    )
+
+    private fun mapNodesModelToDbModel(nodes: MutableList<Node>): MutableList<NodeDbModel> {
+        return nodes.map {
+            mapModelToDbModel(it)
+        }.toMutableList()
+    }
+
+    private fun mapNodesDbModelToModel(nodesDbModel: MutableList<NodeDbModel>): MutableList<Node> {
+        return nodesDbModel.map {
+            mapDbModelToModel(it)
+        }.toMutableList()
+    }
+}
